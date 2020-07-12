820b5914272bfaf6e589dfaaa943527e6a4d8a2c
Sean Schwabenlander <sean@schwabenlander.com>
2020-07-12 16:07:34 -0500

    Adding data services to client project

---
+++ Client/Services/CompanyDataService.cs
@@ -0,0 +1,42 @@
+﻿using JobJournal.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace JobJournal.Client.Services
+{
+    public class CompanyDataService : ICompanyDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CompanyDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<CompanyDTO>> GetCompaniesAsync(Guid userId)
+        {
+            return await _httpClient.GetFromJsonAsync<List<CompanyDTO>>($"api/company/all/{userId}");
+        }
+
+        public async Task<CompanyDTO> GetCompanyAsync(Guid id)
+        {
+            return await _httpClient.GetFromJsonAsync<CompanyDTO>($"api/company/{id}");
+        }
+
+        public async Task<CompanyDTO> PostCompanyAsync(CompanyDTO company)
+        {
+            var response = await _httpClient.PostAsJsonAsync<CompanyDTO>("api/company", company);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<CompanyDTO>();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Client/Services/ICompanyDataService.cs
@@ -0,0 +1,15 @@
+﻿using JobJournal.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobJournal.Client.Services
+{
+    public interface ICompanyDataService
+    {
+        Task<IEnumerable<CompanyDTO>> GetCompaniesAsync(Guid userId);
+
+        Task<CompanyDTO> GetCompanyAsync(Guid id);
+    }
+}
